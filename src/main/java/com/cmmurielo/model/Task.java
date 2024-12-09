bfd0fa756aa31ed6d6966bad250e6d9483da2da0
Carlos Mario Muriel Galvis <cmmurielo@gmail.com>
2024-12-08 22:06:31 -0500

    metodo Task.toString ajustado

--- src/main/java/com/cmmurielo/model/Task.java
+++ src/main/java/com/cmmurielo/model/Task.java
@@ -6,68 +6,70 @@ import com.cmmurielo.utils.Utilities;
 import java.time.LocalDateTime;
 
 public class Task {
-    private int id;
-    private String description;
-    private boolean isCompleted;
-    private String created;
-    private boolean active;
+	private int id;
+	private String description;
+	private boolean isCompleted;
+	private String created;
+	private boolean active;
 
-    public Task(String description) {
-        Utilities utilities = new Utilities();
-        this.description = description;
-        this.created = utilities.parseLocalDataTimeToString(LocalDateTime.now(), Constants.DATE_FORMAT_YYYY_MM_DD_HH_MM_SS_SSSSSS);
-        this.isCompleted = false;
-        this.active = true;
-    }
+	public Task(String description) {
+		Utilities utilities = new Utilities();
+		this.description = description;
+		this.created = utilities.parseLocalDataTimeToString(LocalDateTime.now(),
+				Constants.DATE_FORMAT_YYYY_MM_DD_HH_MM_SS_SSSSSS);
+		this.isCompleted = false;
+		this.active = true;
+	}
 
-    public Task(int id, String description, boolean isCompleted, String created, boolean active) {
-        this.id = id;
-        this.description = description;
-        this.isCompleted = isCompleted;
-        this.created = created;
-        this.active = active;
-    }
+	public Task(int id, String description, boolean isCompleted, String created, boolean active) {
+		this.id = id;
+		this.description = description;
+		this.isCompleted = isCompleted;
+		this.created = created;
+		this.active = active;
+	}
 
+	public int getId() {
+		return id;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public boolean getCompleted() {
+		return isCompleted;
+	}
 
-    public boolean getCompleted() {
-        return isCompleted;
-    }
+	public void setCompleted(boolean isCompleted) {
+		this.isCompleted = isCompleted;
+	}
 
-    public void setCompleted(boolean isCompleted) {
-        this.isCompleted = isCompleted;
-    }
+	public String getCreated() {
+		return created;
+	}
 
-    public String getCreated() {
-        return created;
-    }
+	public void setCreated() {
+		Utilities utilities = new Utilities();
+		this.created = utilities.parseLocalDataTimeToString(LocalDateTime.now(),
+				Constants.DATE_FORMAT_YYYY_MM_DD_HH_MM_SS_SSSSSS);
+	}
 
-    public void setCreated() {
-        Utilities utilities = new Utilities();
-        this.created = utilities.parseLocalDataTimeToString(LocalDateTime.now(), Constants.DATE_FORMAT_YYYY_MM_DD_HH_MM_SS_SSSSSS);
-    }
+	public boolean getActive() {
+		return active;
+	}
 
-    public boolean getActive() {
-        return active;
-    }
+	public void setActive(boolean active) {
+		this.active = active;
+	}
 
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Task ID: %d\nDescription: %s\nCompleted: %s\nCreated: %s\n", id, description, isCompleted ? "Yes" : "No", created);
-    }
+	@Override
+	public String toString() {
+		return String.format("Task ID: %d\nDescription: %s\nCompleted: %s\nCreated: %s\n", id, description,
+				isCompleted ? "Completada" : "Sin completar", created);
+	}
 }
